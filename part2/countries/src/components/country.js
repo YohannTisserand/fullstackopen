fec66e628af7598d17afe97806c5aa1951ab6a0e
YohannTisserand <tisserand.yohann@gmail.com>
2022-03-18 10:48:50 +0000

    countries done

--- part2/countries/src/components/country.js
+++ part2/countries/src/components/country.js
@@ -5,14 +5,14 @@ import Weather from './weather'
 const Country = ({country}) => {
   return (
     <div>
-        <h1>{country.name.common}</h1>
-        <p>capital: {country.capital}</p>
-        <p>population: {country.population}</p>
-        <h2>Spoken languages</h2>
-       <Languages country={country} />
-       <img src={country.flags.png} alt="Country flag"></img>
-       <Weather city={country.capital} />
-      </div>
+      <h1>{country.name.common}</h1>
+      <p>capital: {country.capital}</p>
+      <p>population: {country.population}</p>
+      <h2>Spoken languages</h2>
+      <Languages country={country} />
+      <img src={country.flags.png} alt="Country flag"></img>
+      <Weather city={country.capital} />
+    </div>
   )
 }
 
