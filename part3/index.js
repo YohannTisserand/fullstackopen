d34edfb2c98a5e97b22dfffae43698ef3ecde549
YohannTisserand <tisserand.yohann@gmail.com>
2022-03-21 21:26:49 +0000

    database validators

--- part3/index.js
+++ part3/index.js
@@ -54,28 +54,20 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const person = {
-    name: request.body.name,
-    number: request.body.number,
-  };
+  const { name, number  } = request.body
 
-  Person.findByIdAndUpdate(request.params.id, person, {
-    new: true,
-  })
+  Person.findByIdAndUpdate(request.params.id,
+    { name, number },
+    {new: true, runValidators: true, context: 'query'}
+    )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
     .catch((error) => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-
-  if (body.name === "") {
-    return response.status(400).json({
-      error: 'name missing'
-    })
-  }
 
   const person = new Person({
     name: body.name,
@@ -100,6 +92,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
